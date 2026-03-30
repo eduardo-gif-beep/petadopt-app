ab674e6fae06972479d438d3351c99c8a93021c6
Eduardo-gif-beep <st4111@utr.edu.mx>
2026-03-29 19:41:28 -0600

    View and ViewModel from Login updated

---
+++ src/viewmodels/useLogin.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Alert } from "react-native";
+import StorageService from "../helpers/StorageService";
+
+export const useLogin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        if (!StorageService.validate('email', email)) {
+            Alert.alert('Error', 'Correo inválido');
+            return;
+        }
+
+        if (!StorageService.validate('password', password)) {
+            Alert.alert('Error', 'Contraseña inválida (mínimo 8 caracteres, mayúscula, número)');
+            return;
+        }
+
+        // MOCK LOGIN
+        const fakeToken = "abc123TOKEN";
+
+        await StorageService.saveToken('userToken', fakeToken);
+
+        Alert.alert('Success', 'Login exitoso');
+        setEmail('');
+        setPassword('');
+    };
+
+    return {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        handleLogin
+    };
+};
--- src/views/PantallaLogin.js
+++ src/views/PantallaLogin.js
@@ -1,70 +1,42 @@
-import { Button, StyleSheet, Text, TextInput, View, Switch } from 'react-native';
-import { useRegister } from '../viewmodels/useRegister';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useLogin } from '../viewmodels/useLogin';
 
-const PantallaRegister = () => {
-
+const PantallaLogin = () => {
+    //deconstruir nuestro hook
     const {
-        name,
-        setName,
-        lastName,
-        setLastName,
         email,
         setEmail,
         password,
         setPassword,
-        tienePatio,
-        setTienePatio,
-        handleRegister
-    } = useRegister();
-
+        handleLogin
+    } = useLogin();
+    
     return(
         <View style={styles.contenedor}>
-            <Text style={styles.titulo}>Registro</Text>
+            <Text style={styles.titulo}>Iniciar Sesion</Text>
 
             <TextInput
-                style={styles.input}
-                placeholder="Nombre"
-                value={name}
-                onChangeText={setName}
+            style={styles.input}
+            placeholder="Correo Electronico"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
             />
 
             <TextInput
-                style={styles.input}
-                placeholder="Apellido"
-                value={lastName}
-                onChangeText={setLastName}
+            style={styles.input}
+            placeholder="Contraseña"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={true}
+            autoCapitalize="none"
             />
-
-            <TextInput
-                style={styles.input}
-                placeholder="Correo Electronico"
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-
-            <TextInput
-                style={styles.input}
-                placeholder="Contraseña"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={true}
-                autoCapitalize="none"
-            />
-
-            <View style={styles.switchContainer}>
-                <Text>¿Tienes patio?</Text>
-                <Switch
-                    value={tienePatio}
-                    onValueChange={setTienePatio}
-                />
-            </View>
 
             <Button
-                title="Registrarse"
-                onPress={handleRegister}
-                color='#007bff'
+            title="Iniciar sesion"
+            onPress={handleLogin}
+            color='#28a745'
             />
         </View>
     );
@@ -86,20 +58,12 @@ const styles = StyleSheet.create({
     },
     input: {
         height: 50,
-        borderWidth: 1,
         borderColor: "#ccc",
         borderRadius: 8,
         paddingHorizontal: 15,
         marginBottom: 15,
         backgroundColor: '#ffffff',
         fontSize: 16
-    },
-    switchContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 20
     }
 });
-
-export default PantallaRegister;
+export default PantallaLogin;
